979cd83bcccc6268dc4655343f8a33e9e42a1b39
rajashekarcs2023 <vrajashekar2022mscs@gmail.com>
2024-10-08 22:34:14 -0700

    counter done

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('hello world')
+let count = 10;
+let movies = ["DDLJ","12 Angry men","KGF","Matrix"];
+
+let countEl = document.getElementById("count");
+let addBtnEl = document.getElementById("add__btn");
+let subBtnEl = document.getElementById("sub__btn");
+
+const incrementCount = () => {
+    count++;
+    countEl.innerHTML = count;
+
+};
+
+function decrementCount(){
+    count--;
+    countEl.innerHTML = count;
+};
+
+
+const displayMovies = () => {};
+
+document.addEventListener("DOMContentLoaded",() => {
+    countEl.innerHTML = count;
+    displayMovies();
+});
+
+addBtnEl.addEventListener("click",incrementCount);
+subBtnEl.addEventListener("click",decrementCount);
